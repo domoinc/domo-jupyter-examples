6b4a88a8da28bf0e9205b9a8f099cbcd0dfa0b07
Nolan.Landefeld@domo.com <Nolan.Landefeld@domo.com>
2023-10-31 16:20:29 -0600

    update hugging face example

--- create-model/huggingface-translation-example/translation_model.py
+++ create-model/huggingface-translation-example/translation_model.py
@@ -1,11 +1,14 @@
 from transformers import pipeline
+import pathlib
+import json
 
 class EndpointHandler():     
     def __init__(self):         
         #initialize model  
-        self.pipe = pipeline("translation", model="model-t5-base", tokenizer="tokenizer-t5-base")
+        path = pathlib.Path(__file__).parent.resolve()
+        self.pipe = pipeline("translation", model=f"{path}/model-t5-base", tokenizer=f"{path}/tokenizer-t5-base")
         pass
         
     def invoke(self, data, **kwargs):         
-        return self.pipe(data)
+        return json.dumps(self.pipe(data))
 
